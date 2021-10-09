ab38dce2d9bedc78a9f626ac2faebb5a6f4f409b
JeanSar <jeansar504@gmail.com>
2021-10-09 20:03:11 +0200

    Deco working fine :D

--- src/main/java/fr/univlyon1/m1if/m1if03/servlets/DecoServlet.java
+++ src/main/java/fr/univlyon1/m1if/m1if03/servlets/DecoServlet.java
@@ -1,5 +1,7 @@
 package fr.univlyon1.m1if.m1if03.servlets;
 
+import fr.univlyon1.m1if.m1if03.utils.CandidatListGenerator;
+
 import javax.servlet.http.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -8,23 +10,25 @@ import java.io.*;
 
 @WebServlet(name = "Deco", value = "/deco")
 public class DecoServlet extends HttpServlet {
+
  
-    private String message;
- 
-    public void init() throws ServletException {
-       // Do required initialization
-       message = "Déconnecter l'utilisateur en supprimant ses attributs de session et de le renvoyer vers la page d'accueil";
+    public void init(ServletConfig config) throws ServletException {
+        // Cette instruction doit toujours être au début de la méthode init() pour pouvoir accéder à l'objet config.
+        super.init(config);
+        //ServletContext context = config.getServletContext();
     }
  
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-       throws ServletException, IOException {
-       
-       // Set response content type
-       response.setContentType("text/html");
- 
-       // Actual logic goes here.
-       PrintWriter out = response.getWriter();
-       out.println("<h1>" + message + "</h1>");
+       throws IOException {
+        try {
+            HttpSession session = request.getSession(false); //on récupère la session sans la créer
+            session.invalidate();
+            response.sendRedirect("index.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erreur dans la récupération de la liste des candidats.");
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
  
     public void destroy() {
