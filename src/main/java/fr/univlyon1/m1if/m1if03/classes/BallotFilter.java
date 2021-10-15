6a0812c94c7c5d083be3584fb92f82ff6e0897a6
p1805596 <hamza.touhardji@etu-lyon1.fr>
2021-10-15 19:33:25 +0200

    debut du mcv

---
+++ src/main/java/fr/univlyon1/m1if/m1if03/classes/BallotFilter.java
@@ -0,0 +1,30 @@
+package fr.univlyon1.m1if.m1if03.classes;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter(filterName = "BallotFilter" , urlPatterns="/*")
+public class BallotFilter extends HttpFilter {
+    ServletContext context;
+
+    public void init(FilterConfig config) throws ServletException {
+        super.init(config);
+        context = config.getServletContext();
+    }
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/fr/univlyon1/m1if/m1if03/servlets/Controller.java
@@ -0,0 +1,40 @@
+package fr.univlyon1.m1if.m1if03.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Controller", urlPatterns = {"/election/*"})
+public class Controller extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        //if (path.toString().contains("secondPage")) {}
+        System.out.println("\nPath1 : " + path);
+        System.out.println(request.getContextPath());
+        System.out.println(request.getRequestURI());
+        System.out.println(request.getHeader("Referer"));
+        RequestDispatcher dispatcher = request.getRequestDispatcher("resultats");
+        //dispatcher.forward(request, response);
+
+        //response.sendRedirect("resultats");
+    }
+}
