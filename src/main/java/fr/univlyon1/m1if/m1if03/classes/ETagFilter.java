8fbbdd0fbf806062f2863c48462719c9489badd2
JeanSar <jeansar504@gmail.com>
2021-10-17 21:25:03 +0200

    ETag

--- src/main/java/fr/univlyon1/m1if/m1if03/classes/ETagFilter.java
+++ src/main/java/fr/univlyon1/m1if/m1if03/classes/ETagFilter.java
@@ -8,12 +8,15 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
 @WebFilter(filterName = "ETagFilter" , urlPatterns="/election/vote/ballot")
 public class ETagFilter extends HttpFilter {
     ServletContext context;
+    String lastETagBallot = null;
+
 
     public void init(FilterConfig config) throws ServletException {
         super.init(config);
@@ -21,7 +24,30 @@ public class ETagFilter extends HttpFilter {
     }
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = request.getSession(false);
+        if (request.getMethod().equals("GET")) {
+            String lastETagFromBrowser = request.getHeader("If-None-Match");
+            String lastETagFromServer = lastETagBallot;
+
+            if (lastETagFromServer.equals(lastETagFromBrowser)) {
+                //setting 304 and returning with empty body
+                System.out.println("\n NOT MODIFIED : browser ETag match server one");
+                response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+                return;
+            }
+            System.out.println("\n MODIFIED : server update client and ETag");
+            System.out.print(lastETagFromServer);
+            response.addHeader("ETag", getETag());
+
+        } else { //if c'est une requete POST
+            System.out.println("\n MODIFIED : client update server and ETag");
+            lastETagBallot = getETag();
+        }
         chain.doFilter(request, response);
+    }
+
+    private String getETag () {
+        return "\"version1\"";
     }
 
     public void destroy() {
