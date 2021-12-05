45d63fd1fd5b25f338efe406b8a6ef65304a6efd
JeanSar <jeansar504@gmail.com>
2021-12-05 23:58:42 +0100

    Fixing some misc (press F)

--- src/main/java/fr/univlyon1/m1if/m1if03/servlets/UsersController.java
+++ src/main/java/fr/univlyon1/m1if/m1if03/servlets/UsersController.java
@@ -1,7 +1,6 @@
 package fr.univlyon1.m1if.m1if03.servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.univlyon1.m1if.m1if03.classes.User;
 import fr.univlyon1.m1if.m1if03.utils.ElectionM1if03JwtHelper;
 
 import javax.servlet.ServletConfig;
@@ -46,29 +45,22 @@ public class UsersController extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String path = req.getRequestURI().substring(req.getContextPath().length());
         String subPath = path.substring(7); // on enlève /candidats
         switch (subPath){
             case "login":
                 String login = req.getParameter("login");
-                String nom = req.getParameter("nom");
+                //String nom = req.getParameter("nom");
                 boolean isAdmin = Objects.equals(req.getParameter("admin"), "on");
 
                 if (login == null || login.equals("")) {
                     resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Paramètres de la requète non acceptables."); //Bad request
                 } else {
-                    resp.setHeader("location", req.getRequestURI() + "/" + login);
+                    resp.setHeader("location", req.getRequestURI() + "/" + login.replace(' ', '_'));
                     String token = ElectionM1if03JwtHelper.generateToken(req.getParameter("login"), isAdmin, req);
                     resp.setHeader("Authorization", "Bearer " + token);
                     resp.setStatus(HttpServletResponse.SC_OK);
-
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    User user = new User(login, nom, isAdmin);
-                    String jsonUser = objectMapper.writeValueAsString(user);
-                    resp.setContentType("application/json");
-                    resp.getWriter().print(jsonUser);
-                    resp.getWriter().flush();
                 }
                 break;
             case "logout":
@@ -85,16 +77,10 @@ public class UsersController extends HttpServlet {
                     resp.setStatus(HttpServletResponse.SC_OK);
                     resp.setContentType("application/json" + "; charset=utf-8");
                     resp.getWriter().write("Successful operation");
+                    resp.getWriter().flush();
                 } catch(Exception e) {
                     //Invalid token.
                     resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Utilisateur non authentifié");
-                }
-                break;
-            case "user":
-                if (req.getParameter("nom") != null && !(req.getParameter("nom").equals(""))) {
-                    User user = (User) req.getSession().getAttribute("user");
-                    user.setNom(req.getParameter("nom"));
-                    req.getRequestDispatcher("/WEB-INF/components/electionHome.jsp").forward(req, resp);
                 }
                 break;
             default:
@@ -111,8 +97,14 @@ public class UsersController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        System.out.println("Controller Users = "+ path);
+        String subPath = path.substring(8).replace('_', ' '); // on enlève /users/: et on remplace les "_" par des " "
+        System.out.println("le sous path controller = "+ subPath);
+
         ObjectMapper om = new ObjectMapper();
-        String monToken = ElectionM1if03JwtHelper.generateToken(request.getParameter("nom"), false, request);
+        String monToken = ElectionM1if03JwtHelper.generateToken(subPath, false, request);
         String json = om.writeValueAsString(monToken);
         response.setContentType("application/json");
         response.getWriter().print(json);
